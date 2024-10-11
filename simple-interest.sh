280bf7819487b621f9b630732e188626a072f2e7
Armelio Nofulla <146014177+Armelionofulla@users.noreply.github.com>
2024-10-11 16:12:13 +0200

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,8 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
-   # Additional Authors:
-   # <your GitHub username>
+   
+   # Armelionofulla
 
    # Input:
    # p, principal amount
